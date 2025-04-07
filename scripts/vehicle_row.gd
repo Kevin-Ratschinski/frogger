d4158b35f7da76fb7340f655807b94d6f89cf650
Kevin-Ratschinski <Kevin05774@hotmail.de>
2025-04-07 19:30:34 +0200

    some improvements

--- scripts/vehicle_row.gd
+++ scripts/vehicle_row.gd
@@ -13,15 +13,15 @@ var lane_width: float = 32.0
 @onready var spawn_timer: Timer = $SpawnTimer
 
 func _ready() -> void:
-	spawn_car()
+	spawn_vehicle()
 	spawn_timer.wait_time = randf_range(min_spawn_interval, max_spawn_interval)
 	spawn_timer.start()
 
 func _on_spawn_timer_timeout() -> void:
-	spawn_car()
+	spawn_vehicle()
 	spawn_timer.wait_time = randf_range(min_spawn_interval, max_spawn_interval)
 
-func spawn_car() -> void:
+func spawn_vehicle() -> void:
 	assert(vehicle_scene, "No vehicle scene added")
 	
 	if vehicles.get_child_count() >= max_vehicles:
@@ -31,7 +31,7 @@ func spawn_car() -> void:
 	vehicles.add_child(vehicle)
 	
 	var screen_width = get_viewport_rect().size.x
-	var start_x = -20 if direction == 1 else screen_width + 20
+	var start_x = -20.0 if direction == 1 else screen_width + 20
 	
 	var y_offset = randf_range(-2, 2)
 	vehicle.position = Vector2(start_x, lane_width / 2 + y_offset)
