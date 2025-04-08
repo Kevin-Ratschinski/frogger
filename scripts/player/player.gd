6c81bc3ca96014ef5dd9c2972cd67a2a31b75e12
Kevin-Ratschinski <Kevin05774@hotmail.de>
2025-04-08 15:28:43 +0200

    added finite state machine for safe zones

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -1,15 +1,28 @@
 class_name Player extends CharacterBody2D
 
+@export var GRACE_PERIOD_TIME: int = 50
+
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 signal new_max_row_reached
 
+enum SafetyState { UNSAFE, SAFE, GRACE_PERIOD }
+var safety_state := SafetyState.UNSAFE
+
 var start_position: Vector2 = Vector2(304, 338)
 var step_size: int = 32
 var current_row: int = 0
-var max_row : int = 0
+var max_row: int = 0
+
+# private variables
+var _safe_until_time := 0
 
 func _process(_delta: float) -> void:
+	if safety_state == SafetyState.GRACE_PERIOD:
+		if Time.get_ticks_msec() > _safe_until_time:
+			print("Set Player State: UNSAFE")
+			safety_state = SafetyState.UNSAFE
+	
 	if Input.is_action_just_pressed("move_up"):
 		move_up()
 	elif Input.is_action_just_pressed("move_down"):
@@ -53,7 +66,29 @@ func move_right() -> void:
 	move_and_collide(Vector2(step_size, 0))
 	animation_player.play("jump_right")
 
-func reset_player():
-	global_position = start_position
-	current_row = 0
-	max_row = 0
+func reset():
+	set_process(false)
+	await get_tree().create_timer(0.2).timeout
+	Global.lives -= 1
+	if Global.lives > 0:
+		global_position = start_position
+		current_row = 0
+		max_row = 0
+		await get_tree().create_timer(0.2).timeout
+		safety_state = SafetyState.UNSAFE
+		set_process(true)
+	else:
+		queue_free()
+
+func update_safety_status(new_status: bool):
+	if new_status:
+		safety_state = SafetyState.SAFE
+		print("Set Player State: SAFE")
+		_safe_until_time = Time.get_ticks_msec() + GRACE_PERIOD_TIME
+	else:
+		if safety_state == SafetyState.SAFE:
+			print("Set Player State: GRACE_PERIOD")
+			safety_state = SafetyState.GRACE_PERIOD
+
+func is_player_safe() -> bool:
+	return safety_state != SafetyState.UNSAFE
