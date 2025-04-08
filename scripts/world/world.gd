0df18fc6a28fad40afe6277716b90d8beb4d39d3
Kevin-Ratschinski <Kevin05774@hotmail.de>
2025-04-08 15:32:03 +0200

    added new created scenes to world and added death zones

--- scripts/world/world.gd
+++ scripts/world/world.gd
@@ -4,3 +4,15 @@ extends Node2D
 
 func _on_player_new_max_row_reached() -> void:
 	Global.score += 10
+
+
+func _on_death_zone_left_body_entered(body: Node2D) -> void:
+	if body is Player:
+		var player := body
+		player.reset()
+
+
+func _on_death_zone_right_body_entered(body: Node2D) -> void:
+	if body is Player:
+		var player := body
+		player.reset()
