11f3d9c2bdff0b8e026f27e68fff68fb3c07ad1b
Kevin-Ratschinski <Kevin05774@hotmail.de>
2025-04-25 10:18:42 +0200

    refactored lilypad handling

--- scripts/world/world.gd
+++ scripts/world/world.gd
@@ -1,31 +1,35 @@
 extends Node2D
 
+@export var lilypads: Array[Lilypad]
+
 @onready var ui: UI = $UI
-@onready var lilypad_1: Lilypad = $Lilypad1
-@onready var lilypad_2: Lilypad = $Lilypad2
-@onready var lilypad_3: Lilypad = $Lilypad3
-@onready var lilypad_4: Lilypad = $Lilypad4
-@onready var lilypad_5: Lilypad = $Lilypad5
 @onready var game_over_container: CenterContainer = $UI/GameOverContainer
 @onready var scored_points_label: Label = $UI/GameOverContainer/VBoxContainer/ScoredPointsLabel
 
+var rng := RandomNumberGenerator.new()
+
 func _ready() -> void:
+	rng.randomize()
 	set_random_lilypad_positions()
 	game_over_container.visible = false
 
 func set_random_lilypad_positions() -> void:
-	var lilypad_positions = get_lilypad_positions(20, Vector2(16, 16), 32)
+	var lilypad_positions = get_lilypad_positions(10, Vector2(16, 16), 64)
 	
-	for lilypad: Lilypad in [lilypad_1, lilypad_2, lilypad_3, lilypad_4, lilypad_5]:
-		var random_index := randi() % lilypad_positions.size()
-		var random_rotation := randf_range(0, 360)
+	if lilypad_positions.size() < lilypads.size():
+		push_error("Not enough positions for all Lilypads!")
+		return
+	
+	for lilypad in lilypads:
+		var random_index := rng.randi_range(0, lilypad_positions.size() - 1)
+		var random_rotation := rng.randf_range(0, 360)
 		lilypad.position = lilypad_positions[random_index]
 		lilypad.lilypad_sprite.rotation_degrees = random_rotation
 		lilypad_positions.remove_at(random_index)
 
 func get_lilypad_positions(count: int, start: Vector2, step: int) -> Array[Vector2]:
 	var positions: Array[Vector2] = []
-	for i in count:
+	for i in range(count):
 		positions.append(start + Vector2(step * i, 0))
 	return positions
 
